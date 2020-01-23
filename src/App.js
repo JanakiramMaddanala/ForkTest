356edf36f95507ea4183af87099acdae20d109f0
Ram <jmaddanala@deloitte.com>
2020-01-23 19:21:58 +0530

    Implement routing using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
-function App() {
+function HatsPage(props) {
+  console.log(props);
   return (
     <div>
-      <HomePage />
+      <h1>Hats Page</h1>
     </div>
-  );
+  )
+}
+
+function JacketsPage(props) {
+  console.log(props);
+  return (
+    <div>
+      <h1>Jackets Page</h1>
+    </div>
+  )
+}
+
+
+
+function App() {
+    return (
+        <div>
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/shop/hats" component={HatsPage} />
+                <Route path="/shop/jackets" component={JacketsPage} />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
